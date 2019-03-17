0cf72bcd0b2fd13d4ba9799aff5242071c7772ef
Matmalecki <mmateusz314@gmail.com>
2019-03-17 12:02:46 +0100

    preparation for testing

---
+++ DotNet/Data/Migrations/20190317104730_m1.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNet.Data.Migrations
+{
+    public partial class m1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Book",
+                maxLength: 200,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Book",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Country",
+                value: "Usa");
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Country",
+                value: "Usa");
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Country",
+                value: "Usa");
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Country",
+                value: "France");
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Country",
+                value: "Usa");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Genre",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 200,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Country",
+                table: "Book",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 1,
+                column: "Country",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 2,
+                column: "Country",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 3,
+                column: "Country",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 4,
+                column: "Country",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "Book",
+                keyColumn: "ID",
+                keyValue: 5,
+                column: "Country",
+                value: null);
+        }
+    }
+}
